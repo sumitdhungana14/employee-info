6c15775b370605f722f72ec99302f464cd4b25ae
sumitdhungana14 <sumeetdhungana74@gmail.com>
2020-01-08 10:35:17 +0545

    Updates

---
+++ precache-manifest.97e97005d4330f3199b7df76ffa9b5e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b58bb2950bd77d878885c999d996b286",
+    "url": "/employee-info/index.html"
+  },
+  {
+    "revision": "daac4bab390e2dbbb866",
+    "url": "/employee-info/static/css/main.06a0dcdf.chunk.css"
+  },
+  {
+    "revision": "9918af0d14cc4339530f",
+    "url": "/employee-info/static/js/2.f6e86148.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/employee-info/static/js/2.f6e86148.chunk.js.LICENSE"
+  },
+  {
+    "revision": "daac4bab390e2dbbb866",
+    "url": "/employee-info/static/js/main.683569b8.chunk.js"
+  },
+  {
+    "revision": "d2824789e4cbcec79af1",
+    "url": "/employee-info/static/js/runtime-main.664f4494.js"
+  }
+]);
